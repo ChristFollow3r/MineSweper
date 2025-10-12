19051914bca0c2661e202e7ef4bfc4ce6bf17d84
Nil <nilbadia@gmail.com>
2025-10-12 08:49:41 +0200

    Made the visible grid

--- Minesweeper.cpp
+++ Minesweeper.cpp
@@ -10,7 +10,9 @@ int main()
     int mines;
     const int maxRows = 10;
     const int maxColumns = 10; 
-    char grid[maxRows][maxColumns];  
+    char grid[maxRows][maxColumns];
+    char visibleGrid[maxRows][maxColumns]; 
+
    
     // Ask the user how many rows and columns.
     do
@@ -21,7 +23,7 @@ int main()
         std::cout << "How many columns do you want the game to have (min 2, max 10): ";
         std::cin >> columns;
 
-        std::cout << "How many mines do you want in the grid (only as much as spaces the grid has please): ";
+        std::cout << "How many mines do you want in the grid (only as much as spaces the grid has please): "; // Fix this (if the user enters more bombs than space has the grid the game will break).
         std::cin >> mines; 
 
     } while (rows > 10 || rows < 2 || columns > 10 || columns < 2);
@@ -37,11 +39,20 @@ int main()
 
     }
 
+    // Visible grid
+
+    for (int i = 0; i < rows; i++)  
+    {
+        for (int j = 0; j < columns; j++)
+        {
+           visibleGrid[i][j] = '#'; 
+        }
+
+    }
+
     //Nukes
 
     std::srand(time(0)); 
-
-  
   
     for (size_t placedMines = 0; placedMines < mines;) // I should've done this with a while loop but its already done so it's staying like this
 
@@ -58,19 +69,18 @@ int main()
 
     }
     
-
     // This prints the grid!
 
     for (size_t i = 0; i < rows; i++) 
     {
         for (size_t j = 0; j < columns; j++)
         {
-            std::cout << grid[i][j];
+            std::cout << visibleGrid[i][j];
         }
         std::cout << std::endl; // So far so good :)
     }
-
-    // Now I have to make it so that the grid only shows '#' (spoiler alert, idk how to do that (for now)).
+    
+    // I think I did not
     // I have to make a row of numbers and a row of letters above the grid so the player can properly check for mines.
     // Reveal mines mechanic.
     // The user can't play the game!
